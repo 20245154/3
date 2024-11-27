d5544eef63bee2b9c1abffe6561a4da24b575f44
20245154 <kamsh1119@naver.com>
2024-11-27 13:48:55 +0900

    3
    3

---
+++ intersection.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int i= 0;
+
+    int a[3] = {1,2,3};
+    int b[2] = {3,4};
+
+    while (i<3) {
+        printf("%d", i);
+        i++;
+    }
+
+    return 0;
+}
